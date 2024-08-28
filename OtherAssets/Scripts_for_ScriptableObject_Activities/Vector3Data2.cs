033d171b83efdaf4d8b0e1dac04ac5e50fcf75d0
Christopher Tate <CrustRust15@gmail.com>
2024-08-28 12:13:13 -0600

    added vector 3 data 2

---
+++ OtherAssets/Scripts_for_ScriptableObject_Activities/Vector3Data2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data2 : ScriptableObject
+{
+    public float spawnRangeX;
+    public float spawnRangeY;
+    public float spawnRangeZ;
+}
